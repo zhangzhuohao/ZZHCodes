5f91e67aa2f921c9e7cd67e0b1fd277c3e5edb25
zhangzhuohao <zhangzhuohao@pku.edu.cn>
2024-05-20 18:01:44 +0800

    update

--- Video/CreateVideoFromFrames.m
+++ Video/CreateVideoFromFrames.m
@@ -25,6 +25,7 @@ VidClipName = fullfile(GatherFolder, "RefineVideo.avi");
 
 writerObj = VideoWriter(VidClipName);
 writerObj.FrameRate = 25; % this is 2 x slower
+writerObj.Quality = 100;
 
 % set the seconds per image
 % open the video writer
